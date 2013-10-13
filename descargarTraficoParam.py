cc5cfa0533ad2b9f7be45072560a0afa976ff9cb
Jaime <jaimefmvalbuena@gmail.com>
2013-10-13 18:09:44 +0200

    Fix template dgt redirection

--- descargarTraficoParam.py
+++ descargarTraficoParam.py
@@ -1,6 +1,17 @@
 #! /usr/bin/python
 
-import time, os, sys
+
+"""
+Templates should only contains image dgt url cameras to 'dgt.es/...'
+
+The
+"""
+
+import time, os, sys, re
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+_log = logging.getLogger(__name__)
 
 if len(sys.argv) == 3:
     dest = sys.argv[2]
@@ -9,6 +20,9 @@ if len(sys.argv) == 3:
     print "Storing data in: {0}".format(sys.argv[2])
     os.chdir(sys.argv[2])
     url = sys.argv[1]
+    match = re.match(r'http.*://(?P<url>.*)', url)
+    if match:
+        url = match.group('url')
 else:
     usage = "usage: {0} <url> <path_to_store_data>".format(sys.argv[0])
     exit( usage );
@@ -16,12 +30,17 @@ else:
 print "Connecting to host {0}".format(url)
 os.system('wget -k -H -p {0}'.format(url))
 
+_log.info("URL '%s'" % url)
 # Renombrar cosas
 tNow = time.ctime().replace(':','').split()
 
 newPre = '{0}{1}_{2}/'.format(tNow[0], tNow[2], tNow[3])
 os.mkdir(newPre)
+_log.info("%s folder created" % newPre)
 hostName = url.split('/')[0]
-os.rename(hostName, newPre+hostName)
-os.rename('www.dgt.es', newPre+'www.dgt.es')
+new_name = newPre+hostName
+_log.info("Renamed folder %s by %s" % (hostName, new_name))
+os.rename(hostName, new_name)
+os.rename('dgt.es', newPre+'dgt.es')
+
           
